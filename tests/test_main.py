78e3ac3f5e6e066f6208a81374b43fa1444da334
Колдун Ебучий <andrey11551611@gmail.com>
2023-03-21 20:07:32 +0300

    container

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,3 +3,6 @@ api = 'http://localhost:7000'
 def test_health():
     response =requests.get(f'{api}/health')
     assert response.status_code == 200
+def test_bye():
+    response =requests.get(f'{api}/fir_bye')
+    assert response.status_code == 200
